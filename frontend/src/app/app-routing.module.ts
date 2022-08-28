eddff2d2eea58277307f8b16178277adf194e6e9
kgomotso Motshweni <kgomotso.motshweni@dadevs.co.za>
2022-08-28 11:46:15 +0200

    routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   
- // {path:"",component:HomeComponent},
+{path:'',component:HomeComponent},
 {path: 'about', component: AboutusComponent}];
 
 @NgModule({
